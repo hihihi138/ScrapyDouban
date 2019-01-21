5d9d59b5005977ac3cdd6f64e64b5ca0da76fdf0
Muer <hihihi138@gmail.com>
2019-01-21 16:22:37 +0800

    Update movie_comment.py

--- scrapy/douban/spiders/movie_comment.py
+++ scrapy/douban/spiders/movie_comment.py
@@ -20,14 +20,18 @@ class MovieCommentSpider(Spider):
            (SELECT douban_id FROM comments GROUP BY douban_id) ORDER BY douban_id DESC'
     cursor.execute(sql)
     movies = cursor.fetchall()
-    start_urls = {
-        str(i['douban_id']): ('https://m.douban.com/rexxar/api/v2/movie/%s/interests?count=5&order_by=hot' % i['douban_id']) for i in movies
-    }
+    # start_urls = {
+    #     # str(i['douban_id']): ('https://m.douban.com/rexxar/api/v2/movie/%s/interests?count=5&order_by=hot' % i['douban_id']) for i in movies
+    #     '26394152': ('https://m.douban.com/rexxar/api/v2/movie/26394152/interests?count=20&start=%s&order_by=hot' % i) for i in range(0, 100, 25)
+    # }
+    url_template = 'https://m.douban.com/rexxar/api/v2/movie/26394152/interests?count=20&start={}&order_by=hot'
 
     def start_requests(self):
-        for (key, url) in self.start_urls.items():
+        # for (key, url) in self.start_urls.items():
+        for i in range(500, 60000, 20):
             headers = {
-                'Referer': 'https://m.douban.com/movie/subject/%s/comments' % key
+                # 'Referer': 'https://m.douban.com/movie/subject/%s/comments' % key
+                'Referer': 'https://m.douban.com/movie/subject/26394152/comments'
             }
             bid = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(11))
             cookies = {
@@ -35,7 +39,7 @@ class MovieCommentSpider(Spider):
                 'dont_redirect': True,
                 'handle_httpstatus_list': [302],
             }
-            yield Request(url, headers=headers, cookies=cookies)
+            yield Request(self.url_template.format(i), headers=headers, cookies=cookies)
 
     def parse(self, response):
         if 302 == response.status:
@@ -52,4 +56,4 @@ class MovieCommentSpider(Spider):
                 comment['douban_user_url'] = item['user']['url']
                 comment['content'] = item['comment']
                 comment['votes'] = item['vote_count']
-            yield comment
+                yield comment
